e98cb07764e1898853b1d333a51fe68d6f6dd47c
shahid-mulani_inmobi <shahid.mulani@inmobi.com>
2023-11-01 17:10:30 +0530

    updated image user

--- next.config.js
+++ next.config.js
@@ -3,7 +3,19 @@ const nextConfig = {
     experimental: {
         serverActions: true,
         mdxRs: true,
-        serverComponentsExternalPackages: ['mongoose']
+        serverComponentsExternalPackages: [ 'mongoose' ]
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*'
+            },
+            {
+                protocol: 'http',
+                hostname: '*'
+            }
+        ]
     }
 }
 
